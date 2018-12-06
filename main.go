11adfc477e6e1dee98b0cf40b0ead05027bf695c
dani <dani@patagonia>
2018-12-06 16:43:25 +0100

    Refactoring

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"os"
-
-	c "github.com/dlouvier/playlistback/controllers"
+	"github.com/dlouvier/playlistback/controllers"
 )
 
 func main() {
-	// Importing the package classes
-	c.LoginAPI()
-	fmt.Println("Your access token is: " + os.Getenv("TOKEN"))
-
-	response := c.GetRequests("https://api.spotify.com/v1/users/dlouvier")
-	fmt.Println("Response from API: \n========================================\n\n" + response)
+	controllers.LoginAPI()
+	// lala := controllers.GetTracks("6TPKQeYHfwLDvLhjAVvPz9")
+	// fmt.Println(lala)
 }
